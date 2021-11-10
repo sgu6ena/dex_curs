e2c2ea02dfac8a5139c467403309dbb2ac202801
sgu6ena <issar@admin.idknet.com>
2021-11-10 11:27:05 +0200

    homeWork -> add sum, sort

--- homeWork-2/homeWork.js
+++ homeWork-2/homeWork.js
@@ -1,7 +1,14 @@
 export const sum = (f, s) => {
     //TODO: Должны складываться только строки и числа
     // Постарайтесь сложить как можно больше пар и обойти NaN случаи
-    return f + s;
+    if (isNaN(parseInt(f, 10) + parseInt(s, 10))) {
+        if (parseInt(s, 10)) {
+            return parseInt(s)
+        } else {
+            return parseInt(f)
+        }
+    } else
+        return parseFloat(f) + parseFloat(s);
 };
 
 export const showPrice = (price, discount) => {
@@ -9,10 +16,10 @@ export const showPrice = (price, discount) => {
 
     //TODO: Привести цену к виду: 10.00 р.
     // округлять до копеек в сторону магазина
-    return Math.ceil(discountedPrice*100)/100;
+    return Math.ceil(discountedPrice * 100) / 100;
 };
 
-export const findSuccess = (bankResponse) => {
+export const findSuccess = bankResponse => {
     //TODO: Проверьте что в строке есть 'Success' без учёта регистра
     // приводим обе строки в нижний регистр
     let string = bankResponse.toLowerCase();
@@ -22,27 +29,30 @@ export const findSuccess = (bankResponse) => {
 };
 
 
-export const dateToString = (date) => {
+export const dateToString = date => {
     //TODO: Преобразовать дату к Вчера/Сегодня/Завтра или год:месяц:день
     const today = new Date(); // сегодняшнаяя дата
     const dateItem = new Date(date); // входящая дата
     const days = today.getDate() - dateItem.getDate(); //разница в днях
     // если разница в днях: -1,0,1 + год и месяц совпадают
-    if (Math.abs(days) < 2  &&
-        today.getMonth() == dateItem.getMonth() &&
-        today.getFullYear() == dateItem.getFullYear()) {
-        return days == 0 ? 'сегодня' :
-            days > 0 ? 'вчера':'завтра';
+    if (Math.abs(days) < 2 &&
+        today.getMonth() === dateItem.getMonth() &&
+        today.getFullYear() === dateItem.getFullYear()) {
+        return days === 0 ? 'сегодня' :
+            days > 0 ? 'вчера' : 'завтра';
     } else {
         //год:месяц:день
-        return date.slice(0,10).replaceAll('-',':');
+        return date.slice(0, 10).replaceAll('-', ':');
         //return `${dateItem.getFullYear()}:${dateItem.getMonth()}:${dateItem.getDate()}`;
     }
 
 
 };
 
-export const sort = (data) => {
+export const sort = data => {
     //TODO: Отсортируйте массив строк по алфавиту
-    return data;
+
+    return data.sort((a, b) =>
+        a.toLowerCase().replace('ё', 'е').codePointAt(0) -
+        b.toLowerCase().replace('ё', 'е').codePointAt(0));
 };
