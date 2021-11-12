353a1730acc6facbb94d1f7b582e20bc81383a65
sgu6ena <issar@admin.idknet.com>
2021-11-12 15:04:30 +0200

    theory array

--- independentWork-2/calculator.js
+++ independentWork-2/calculator.js
@@ -5,12 +5,9 @@
 Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
 
 Пример использования:
-
 let calc = new Calculator;
-
 alert( calc.calculate("3 + 7") ); // 10
 Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
-
 Например, давайте добавим умножение *, деление / и возведение в степень **:
 
 let powerCalc = new Calculator;
@@ -27,14 +24,14 @@ alert( result ); // 8
 function Calculator() {
     this.calculate = (str) => {
         this.arg = str.trim().split(' ');
-        if (this.arg.length == 3) {
+        if (this.arg.length === 3) {
             console.log(this.arg);
             if (!isNaN(+this.arg[0]) && !isNaN(+this.arg[2])) {
                 return this[this.arg[1]](+this.arg[0], +this.arg[2]);
             } else {
                 console.warn(`Неверные аргументы: ${str} `)
+                return null;
             }
-
         } else {
             console.warn('Неправильное количество аргументов')
             return null;
@@ -50,7 +47,9 @@ let calc = new Calculator;
 calc.addMethod("-", (a, b) => a - b);
 calc.addMethod("+", (a, b) => a + b);
 
-console.log(calc.calculate("3 - 7 ")); // 10
+console.log(calc.calculate("3 + 7"));
+console.log(calc.calculate("3 + 7"));
+console.log(calc.calculate("ф + и"));
 
 let powerCalc = new Calculator;
 powerCalc.addMethod("*", (a, b) => a * b);
