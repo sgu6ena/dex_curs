b80b4e61000ea98367d34799ea0327eb58bfea79
sgu6ena <sgu6ena@gmail.com>
2021-11-15 00:39:01 +0200

    getCartItemsByDate, repeatOrder +idfix +>500fix +getCartItemsByDate fix

--- homeWork-3/homework.js
+++ homeWork-3/homework.js
@@ -1,25 +1,30 @@
 export const processCartData = (cartData) => {
     cartData.map((item) => {
         const diff = item.oldPrice - item.price;
-        diff > 0 ? (item.discount = diff) : '';
+        return diff > 0 ? (item.discount = diff) : "";
     });
     cartData.map((item) => delete item.oldPrice);
     return cartData;
 };
 
 export const makeCartItemCopy = (cartItem) => {
-    const clone = JSON.parse(JSON.stringify(cartItem));
-    const findItem = clone.findIndex((item) => item.name === "Пицца с анчоусами");
-    clone[findItem]?.addedIngredients.map((item) => (item.count += 1));
+    let clone = JSON.parse(JSON.stringify(cartItem));
+    if (clone.name === "Пицца с анчоусами") {
+        clone.addedIngredients.map(item=> item.count += +1);
+    }
+
     return clone;
 };
 
 export const calcSum = (cartData) => {
     const calcSumType = (cartData, type) => {
         const arr = type ? cartData.filter((item) => item.type === type) : cartData;
-        const sum = arr.reduce((total, item) => total += item.count * item.price, 0);
-        const count = arr.reduce((total, item) => total += item.count, 0);
-        return {sum, count};
+        const sum = arr.reduce(
+            (total, item) => (total += item.count * item.price),
+            0
+        );
+        const count = arr.reduce((total, item) => (total += item.count), 0);
+        return { sum, count };
     };
     return {
         total: calcSumType(cartData),
@@ -30,29 +35,27 @@ export const calcSum = (cartData) => {
 };
 
 export const getCartItemsByDate = (cartData, date) => {
-    //let d = date.slice(0,10);new Date();
+    const d =  new Date(date);
+    const arr = cartData.filter((item) =>
+        new Date(item.date.slice(0,10)).toDateString() === d.toDateString()
+    );
 
-    //TODO: выбрать покупки сделанные за выбранную дату
-    // const dateItem = Date(date);
-    // console.log(cartData.filter(item=> item));
-    // console.log(dateItem.getDate;
-    return cartData;
-};
+    return date ? arr : cartData;
+}
 
 export const repeatOrder = (cartData, date) => {
-    console.log(date);
-    let d = new Date(date);
-    let day = d.getDate();
-    console.log(day);
+    const dateNow = new Date();
+    const arr = cartData.filter((item) => item['date'] === date);
+    const clone = arr.map((i) => makeCartItemCopy(i));
+    let id = cartData.reduce((idmax,item)=>item.id>idmax ? item.id : idmax, cartData[0].id)
+    for (let item of clone) {
+        item.date = dateNow.toISOString();
+        item.id = `${++id}`;
+    }
 
-    //TODO: нужно повторить заказ за выбранную дату
-    // дублиовать соответствующие элементы
-    // поставить в начало спиcка
-    // дату текущую
-    // поменять id на уникальный
-
-    return cartData;
+    return clone.concat(cartData);
 };
+
 
 export const addItem = (cartData, item) => {
     cartData.map((i) => (i.id === item.id ? (i.count += 1) : ""));
@@ -62,7 +65,8 @@ export const addItem = (cartData, item) => {
 export const checkPromo = (cartData) => {
     const sum = calcSum(cartData);
     const total = sum.total.sum > 1000;
-    const oneBigPosition = sum.pizza.sum > 500 || sum.water.sum > 500 || sum.other.sum > 500;
+    const oneBigPosition = !!cartData.filter(item=>item.count*item.price>500).length;
+    //  sum.pizza.sum > 500 || sum.water.sum > 500 || sum.other.sum > 500;
     const notDiscounted = !!cartData.filter((item) => item.discount).length;
-    return {total, oneBigPosition, notDiscounted};
+    return { total, oneBigPosition, notDiscounted };
 };
