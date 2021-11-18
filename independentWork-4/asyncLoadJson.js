82dca227db525bfffb8d29d8094fa486136f85bc
sgu6ena <issar@admin.idknet.com>
2021-11-18 12:21:10 +0200

    add solution

--- independentWork-4/asyncLoadJson.js
+++ independentWork-4/asyncLoadJson.js
@@ -14,3 +14,21 @@ function loadJson(url) {
 
 loadJson('no-such-user.json') // (3)
   .catch(alert); // Error: 404*/
+
+const url = `https://jsonplaceholder.typicode.com/todos/1`;
+const urlError = 'https://jsonplaceholder.typicode.com/todoss/1'
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        const result = await response.json();
+        return JSON.stringify(result);
+    } else
+        return response.status;
+}
+
+let data = await loadJson(url).catch();
+let errorMessage = await loadJson(urlError).catch();
+
+console.log('результат:', data);
+console.log('ошибка:', errorMessage);
