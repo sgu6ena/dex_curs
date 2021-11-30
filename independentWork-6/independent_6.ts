284480c687df0e6851f10c53c8c2559fb8bc1323
sgu6ena <issar@admin.idknet.com>
2021-11-30 18:01:50 +0200

    generics

--- independentWork-6/independent_6.ts
+++ independentWork-6/independent_6.ts
@@ -1,6 +1,6 @@
 interface Rect {
-    readonly id: string
-    color?: string
+    readonly id: string // только для чтения
+    color?: string //необязательный параметр
     size:{
         width: number
         height: number
@@ -25,3 +25,44 @@ const rect2: Rect = {
 }
 
 rect2.color ='black'
+
+//дженерики
+
+function  myFunc<T>(firstParam: T): T{
+    return firstmParam;
+}
+
+myFunc<string>('123');
+myFunc<number>(1);
+
+
+function funcy <T,U> (firstparam:T, secondParam:U): [T,U]{
+    return [firstparam,secondParam];
+}
+
+interface Product{
+    name: string;
+    price:string;
+}
+
+interface Student {
+    name: string;
+}
+
+function getFullInfo <T, K extends keyof T>(entity: T, key: K): T|null;
+
+getFullInfo<Product>(product: Product): Product | null;
+getFullInfo<Student>(student: Student): Student | null;
+
+
+class GenericClass<T>{
+    prop: T;
+    func: (arg: T) => T;
+}
+
+let g = new GenericClass<number>()
+g.prop=3;
+g.func=(param)=>{
+    return param +1;
+}
+console.log(g.func(g.prop));
