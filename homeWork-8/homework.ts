e4ca451db09bf49fc7f3392b06210dbe16288939
sgu6ena <sgu6ena@gmail.com>
2021-12-01 22:59:10 +0200

    add second solution and fix first interfaces

--- homeWork-8/homework.ts
+++ homeWork-8/homework.ts
@@ -9,15 +9,19 @@ interface iStages {
 }
 
 interface iDrive {
-    stages: iStages[];
+    stages?: iStages[];
     supportedEnvironments: string[];
+    power?: number;
+    torque?: number;
+    type?: string;
 }
-interface VehicleType {
+
+type VehicleType = {
     type: string;
     brand: string;
     model: string;
     drive: iDrive;
-}
+};
 export const vehicles: VehicleType[] = VehicleData;
 
 // реализуйте класс "Транспортное средство" и его потомков
@@ -30,16 +34,55 @@ export const vehicles: VehicleType[] = VehicleData;
 //       количество двигателей.
 //   для автомобилей - "Power: {pwr}HP Torque: {Tq}Nm", с выводом соотв. значений
 //   для вертолётов - "Under secret"
+//  class Vehicle extends VehicleType={
+//   getTitle:() string
+//  }
 
 class Vehicle {
+    type: string;
+    brand: string;
+    model: string;
+    env: string[];
+    pwr?: number;
+    tq?: number;
+    stages?: iStages[];
     constructor(vehicle: VehicleType) {
+        this.type = vehicle.type;
+        this.brand = vehicle.brand;
+        this.model = vehicle.model;
+        this.env = vehicle.drive.supportedEnvironments;
+        this.pwr = vehicle.drive.power;
+        this.tq = vehicle.drive.torque;
+        this.stages = vehicle.drive.stages;
     }
 
     getTitle(): string {
-        return "title";
+        console.log(this);
+
+        return `${this.brand} - ${this.model}`;
     }
     getInfo(): string {
-        return "info";
+        if (this.type === "Helicopter") {
+            return `Under secret`;
+        } else {
+            const envInfo: string = `Supported environments: ${this.env.join(
+                ", "
+            )}. `;
+            switch (this.type) {
+                case "Car":
+                    return `${envInfo} Power: ${this.pwr}. HP Torque: ${this.tq}Nm.`;
+                case "Spacecraft":
+                    const cnt: number | undefined = this.stages
+                        ?.map((a) => a.engineCount)
+                        .reduce((total, current) => (total += current), 0);
+                    const calc: number | undefined = this.stages
+                        ?.map((a) => a.engineThrust)
+                        .reduce((total, current) => (total += current), 0);
+                    return `${envInfo} Total thrust: ${calc}kN Engine count: ${cnt}`;
+                default:
+                    return `${envInfo || "неизвестный тип"}`;
+            }
+        }
     }
 }
 
@@ -49,3 +92,4 @@ class Vehicle {
 export function vehicleFabric(vehicle: VehicleType): Vehicle | null {
     return new Vehicle(vehicle);
 }
+
