1165c23271d7a9e73f9f128903dec7eb3c50c1b6
sgu6ena <issar@admin.idknet.com>
2021-12-02 08:27:14 +0200

    add third solution

--- homeWork-8/homework.ts
+++ homeWork-8/homework.ts
@@ -9,15 +9,19 @@ interface iStages {
 }
 
 interface iDrive {
-    stages: iStages[];
+    stages?: iStages[];
     supportedEnvironments: string[];
+    power?: number;
+    torque?: number;
+    type?: string;
 }
-interface VehicleType {
+
+type VehicleType = {
     type: string;
     brand: string;
     model: string;
     drive: iDrive;
-}
+};
 export const vehicles: VehicleType[] = VehicleData;
 
 // реализуйте класс "Транспортное средство" и его потомков
@@ -32,20 +36,59 @@ export const vehicles: VehicleType[] = VehicleData;
 //   для вертолётов - "Under secret"
 
 class Vehicle {
+    readonly type: string;
+    readonly brand: string;
+    readonly model: string;
+    readonly env: string[];
+    readonly pwr?: number;
+    readonly tq?: number;
+    readonly stages?: iStages[];
+    info: string;
+    title: string;
+
     constructor(vehicle: VehicleType) {
+        this.type = vehicle.type;
+        this.brand = vehicle.brand;
+        this.model = vehicle.model;
+        this.env = vehicle.drive.supportedEnvironments;
+        this.pwr = vehicle.drive.power;
+        this.tq = vehicle.drive.torque;
+        this.stages = vehicle.drive.stages;
     }
 
     getTitle(): string {
-        return "title";
+        return `${this.brand} - ${this.model}`;
     }
+
     getInfo(): string {
-        return "info";
+        if (this.type === "Helicopter") {
+            return `Under secret`;
+        } else {
+            const envInfo: string = `Supported environments: ${this.env.join(", ")}.`;
+            switch (this.type) {
+                case "Car":
+                    return `${envInfo} Power: ${this.pwr}. HP Torque: ${this.tq}Nm.`;
+                case "Spacecraft":
+                    const cnt: number | undefined = this.stages
+                        ?.map((stage) => stage.engineCount)
+                        .reduce((total, current) => (total += current), 0);
+                    const calc: number | undefined = this.stages
+                        ?.map((stage) => stage.engineThrust)
+                        .reduce((total, current) => (total += current), 0);
+                    return `${envInfo} Total thrust: ${calc}kN Engine count: ${cnt}`;
+                default:
+                    return `${envInfo || "неизвестный тип"}`;
+            }
+        }
     }
 }
 
-// реализйте функцию конвертации полученнх данных в конечный тип для
+// реализуйте функцию конвертации полученных данных в конечный тип для
 // последующего вывода данных о транспортном средстве
 
 export function vehicleFabric(vehicle: VehicleType): Vehicle | null {
-    return new Vehicle(vehicle);
+    const newVehicle: Vehicle = new Vehicle(vehicle);
+    newVehicle.info = newVehicle.getInfo();
+    newVehicle.title = newVehicle.getTitle();
+    return newVehicle;
 }
